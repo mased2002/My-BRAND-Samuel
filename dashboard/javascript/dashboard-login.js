74a48bd9df014297df58f8a04b19b913faccbd43
mased2002 <edsam2302@gmail.com>
2024-07-12 06:30:56 +0200

    this is it

--- dashboard/javascript/dashboard-login.js
+++ dashboard/javascript/dashboard-login.js
@@ -44,7 +44,7 @@ let submitBtn = document.querySelector(".submit_btn");
 
 let error_email = document.getElementById("error_email");
 let error_password = document.getElementById("error_password");
-getData()
+console.log(getData())
 // Declare isValid outside the event listeners
 let isValid;
 let isValid_pass;
