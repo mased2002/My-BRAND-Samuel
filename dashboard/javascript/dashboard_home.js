947733e62396d873b9486d1d34bb3f12d1045401
mased2002 <edsam2302@gmail.com>
2024-07-17 16:54:33 +0200

    this is basicly not going as ex[pwcted

--- dashboard/javascript/dashboard_home.js
+++ dashboard/javascript/dashboard_home.js
@@ -4,6 +4,7 @@ const comment_container = document.querySelectorAll(".comment_cont")
 const commentator = document.querySelectorAll(".name_of_comment")
 const blog = document.getElementsByClassName("blog_comment_on")
 const comment = document.querySelectorAll("comment_content")
+get_comments();
 
 async function get_comments(){
     const response = await fetch("https://my-brand-samuel-backend.onrender.com/api/comments/getAll",{
@@ -12,6 +13,8 @@ async function get_comments(){
       
       })
       const json = response.json()
+      console.log(json)
+      console.log(response.status)
       if(response.status == 200){
         const comments = json
         if(comments){
