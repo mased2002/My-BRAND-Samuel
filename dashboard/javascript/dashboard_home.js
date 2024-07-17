392f2f1e84c7c93b6e65e33c5880c7029595e761
mased2002 <edsam2302@gmail.com>
2024-07-17 16:44:20 +0200

    trying to make home page active

---
+++ dashboard/javascript/dashboard_home.js
@@ -0,0 +1,33 @@
+// crate elements
+const comment_container = document.querySelectorAll(".comment_cont")
+// const comment = document.getElementsByClassName("comment")
+const commentator = document.querySelectorAll(".name_of_comment")
+const blog = document.getElementsByClassName("blog_comment_on")
+const comment = document.querySelectorAll("comment_content")
+
+async function get_comments(){
+    const response = await fetch("https://my-brand-samuel-backend.onrender.com/api/comments/getAll",{
+        headers:{"Content-Type": "application/json"},
+        method: "GET",
+      
+      })
+      const json = response.json()
+      if(response.status == 200){
+        const comments = json
+        if(comments){
+          comment.forEach((com, index)=> {
+            if(index < comments){
+              com.textContent = comment[index].content
+            }
+          })
+        }
+      }
+
+
+}
+// const comments = get_comments()
+// if(comments){
+//   comment.forEach((com, index) => {
+//     if(index ){}
+//   })
+// }
