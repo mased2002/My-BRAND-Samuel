d17b4045334ca6c009976d3f741b0d31bebfb097
mased2002 <edsam2302@gmail.com>
2024-07-24 21:18:50 +0200

    changing classname to id

--- dashboard/javascript/dashboard_home.js
+++ dashboard/javascript/dashboard_home.js
@@ -6,7 +6,7 @@ const blog = document.querySelectorAll(".blog_comment_on")
 const comment = document.querySelectorAll(".comment_content")
 get_comments();
 get_data();
-const text = document.getElementsByClassName("p.number_messages")
+const text = document.getElementById("number_messages")
 
 async function get_comments(){
     const response = await fetch("https://my-brand-samuel-backend.onrender.com/api/comments/getAll",{
